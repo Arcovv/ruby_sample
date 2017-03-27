f487437673a527178579ebfa4694a395cd716de6
Arco-vv <yiyezhihen@gmail.com>
2017-03-27 09:38:50 +0800

    [Learn] Today examples

---
+++ chapter_7/literal_constructor.rb
@@ -0,0 +1,29 @@
+# 7.1
+str = String.new
+arr = Array.new
+
+# Literal constructor
+
+# String
+"new String"
+'new String'
+
+# Symbol
+:symbol
+:"symbol with spaces"
+
+# Array
+[1,2,3,4,5]
+
+# Hash
+{"New Youk" => "NY", "Oregon" => "OR"}
+
+# Range
+0..9
+0...10
+
+# Regexp
+/([a-z]+)/
+
+# Proc(lambda)
+-> (x, y) { x * y }
