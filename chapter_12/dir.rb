4730f7096a1d88b229b79867f009c1ada408fdfb
Arco-vv <yiyezhihen@gmail.com>
2017-04-20 21:48:56 +0800

    Today examples

--- chapter_12/dir.rb
+++ chapter_12/dir.rb
@@ -9,4 +9,41 @@ p d.entries
 p Dir.entries("../chapter_12")
 
 entries = d.entries
-entries.delete_if
+entries.delete_if { |entry| entry =~ /^\./ }
+entries.map! { |entry| File.join(d.path, entry) }
+entries.delete_if { |entry| !File.file?(entry) }
+print "Total bytes: "
+puts entries.inject(0) { |total, entry| total + File.size(entry) }
+
+puts "------"
+
+p Dir["../chapter_12/*.rb"]
+
+dir = "../chapter_12"
+print "Total bytes: "
+puts entries = Dir["#{dir}/*"]
+  .select { |entry| File.file?(entry) }
+  .inject(0) { |total, entry| total + File.size(entry) }
+
+
+# 12.4.2
+
+newdir = "/tmp/newdir"
+newfile = "newfile"
+Dir.mkdir(newdir)
+Dir.chdir(newdir) do 
+  File.open(newfile, "w") do |f|
+    f.puts "Sample file in new directory"
+  end
+  puts "Current directory: #{Dir.pwd}"
+  puts "Directory listing: "
+  p Dir.entries(".")
+  File.unlink(newfile)
+end
+Dir.rmdir(newdir)
+print "Does #{newdir} still exist? "
+if File.exist?(newdir)
+  puts "YES"
+else
+  puts "NO"
+end
